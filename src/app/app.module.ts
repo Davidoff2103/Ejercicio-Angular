05a5b5ce098a0c5e0081b0b6c4d20aff76530e73
Davidoff2103 <davidoff.d777@gmail.com>
2021-10-19 17:48:00 +0200

    Nuevo modulo coches y componente coche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
 import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { ContadorModule } from './contador/contador.module';
 import { ListadoComponent } from './heroes/listado/listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
     HeroeComponent,
     ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContadorModule
   ],
   providers: [],
-  bootstrap: [HeroeComponent, ContadorComponent, ListadoComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
