c0e82782da255c7f5c7c6e47dad50ff8e91cc3af
Davidoff2103 <davidoff.d777@gmail.com>
2021-10-20 18:33:23 +0200

    Últimos cambios de Parte 3 - Ejercicio 1

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {
+  Component
+} from '@angular/core';
+import {
+  NgForm
+} from '@angular/forms';
 
 @Component({
   selector: 'app-main-page',
@@ -7,39 +11,38 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
-  Personajes: Personaje[] = [personaje1, personaje2, personaje3, personaje4]
-  constructor() { }
-
-  nuevoPersonaje(itemForm: NgForm){
-    let personajeNuevo:Personaje = {
-      nombre:itemForm.value.nombre,
-      poder: itemForm.value.poder
-    }
-    this.Personajes.push(personajeNuevo);
+  personaje1: Personaje = {
+    nombre: 'Goku',
+    poder: 100
   }
-}
 
-let personaje1:Personaje = {
-  nombre:'Goku',
-  poder: 100
-}
+  personaje2: Personaje = {
+    nombre: 'Vegetta',
+    poder: 90
+  };
 
-let personaje2:Personaje = {
-  nombre:'Vegetta',
-  poder: 90
-}
+  personaje3: Personaje = {
+    nombre: 'Freezer',
+    poder: 95
+  };
 
-let personaje3:Personaje = {
-  nombre:'Freezer',
-  poder: 95
-}
+  personaje4: Personaje = {
+    nombre: 'Gohan',
+    poder: 97
+  };
 
-let personaje4:Personaje = {
-  nombre:'Gohan',
-  poder: 97
-}
+  Personajes: Personaje[] = [this.personaje1, this.personaje2, this.personaje3, this.personaje4];
+
+  nuevoPersonaje(itemForm: NgForm) {
+    let personajeNuevo: Personaje = {
+      nombre: itemForm.value.nombre,
+      poder: itemForm.value.poder
+    };
+    this.Personajes.push(personajeNuevo);
+  };
+};
 
 interface Personaje {
   nombre: string,
-  poder: number
+    poder: number
 };
